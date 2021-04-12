8550665f360b7bd4bdecdf6376ea9b710dbe570e
vramos <ramosfonbon@gmail.com>
2021-04-12 12:32:19 -0500

    Icreased JWT timeout

--- src/main/java/org/springframework/samples/petclinic/security/SecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/security/SecurityConfig.java
@@ -69,8 +69,6 @@ public class SecurityConfig {
                                 "/**/*.css",
                                 "/**/*.js"
 
-
-
                         )
                         .antMatchers(
                                 HttpMethod.POST,
--- src/main/java/org/springframework/samples/petclinic/user/UserController.java
+++ src/main/java/org/springframework/samples/petclinic/user/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
                     .map(GrantedAuthority::getAuthority)
                     .collect(Collectors.toList()))
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 600000))
+            .setExpiration(new Date(System.currentTimeMillis() + 6000000))
             .signWith(SignatureAlgorithm.HS512,
                 secretKey.getBytes()).compact();
 
